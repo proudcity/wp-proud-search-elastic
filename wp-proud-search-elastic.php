f18c1ff7bf379bfb0dafae3190b6b8ebcdd04410
Curtis McHale <curtis@curtismchale.ca>
2025-05-06 12:39:04 -0700

    fixing formatting to kill all the errors

--- wp-proud-search-elastic.php
+++ wp-proud-search-elastic.php
@@ -1,5 +1,5 @@
 <?php
-/*
+/**
 Plugin Name:        Proud Search Elastic
 Plugin URI:         http://getproudcity.com
 Description:        ProudCity distribution
@@ -8,11 +8,15 @@ Author:             ProudCity
 Author URI:         http://getproudcity.com
 
 License:            Affero GPL v3
-*/
+
+@package   Wp_Proud_Search_Elastic
+@author    ProudCity <info@proudcity.com>
+@copyright 2025 ProudCity
+**/
 
 // Elastic Search?
-if ( class_exists( 'ElasticPress\Elasticsearch' ) ) {
-  require_once( plugin_dir_path(__FILE__) . 'lib/elasticsearch.class.php' );
+if (class_exists('ElasticPress\Elasticsearch')) {
+    include_once plugin_dir_path(__FILE__) . 'lib/elasticsearch.class.php';
 }
 
 // @NOTE we are dropping the CLI integration for now in favor of using `wp elasticpress`
@@ -24,14 +28,14 @@ if ( class_exists( 'ElasticPress\Elasticsearch' ) ) {
 // }
 
 // Settings page
-if( is_admin() ) {
-  if( class_exists( 'ProudSettingsPage' ) ) {
-    function proud_search_elastic_settings() {
-      require_once( plugin_dir_path(__FILE__) . 'settings/elastic-settings.php' );
+if (is_admin()) {
+    if (class_exists('ProudSettingsPage')) {
+        function proud_search_elastic_settings()
+        {
+            include_once plugin_dir_path(__FILE__) . 'settings/elastic-settings.php';
+        }
+        add_action('init', 'proud_search_elastic_settings');
+    } else {
+        include_once plugin_dir_path(__FILE__) . 'settings/proud-elastic-agent.php';
     }
-    add_action( 'init', 'proud_search_elastic_settings' );
-  }
-  else {
-    require_once( plugin_dir_path(__FILE__) . 'settings/proud-elastic-agent.php' );
-  }
 }
